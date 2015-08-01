3f1190d5fedc5aebc67ad6ba1d85b2bfca0f2140
staceyko <ko.stacey@gmail.com>
2015-08-01 00:09:57 -0400

    done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+    if array.length == 1
+        array.join
+        elsif array.length == 2
+        array.join(" and ")
+        elsif array.length == 3
+        last_element = array[-1]
+        "#{array[0..1].join(", ")}, and #{last_element}"
+        else
+        last_element = array[-1]
+        "#{array[0..-2].join(", ")}, and #{last_element}"
+    end
 end
